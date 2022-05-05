0ef3f52ccec96976d7cb8ef0cae3bae778508a5f
zeeshaninovio <103288137+zeeshaninovio@users.noreply.github.com>
2022-05-05 04:47:31 -0700

    initial commit

---
+++ inc/header.php
@@ -0,0 +1,49 @@
+<header id="header">
+  <!-- Nav Top section   -->
+<section class="top">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6"> </div>
+                <div class="col-md-6"></div>
+            </div>
+        </div>
+    </section>
+<!-- Nav Top Section  -->
+
+<!-- Nav section  -->
+<section class="nav">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6"></div>
+            <div class="col-md-6"></div>
+        </div>
+    </div>
+
+</section>
+
+<div class="canvas-icon text-right">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+<!-- Nav section  -->
+
+<div class="mobile-header navbar-dark">
+        <div class="cancel">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-x"
+                viewBox="0 0 16 16">
+                <path
+                    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+            </svg>
+        </div>
+        <ul class="mobile-nav navbar-nav">
+            <li class="nav-link"><a class="text-black" href="index.php">Home</a></li>
+            <li class="nav-link"><a class="text-black" href="souvenir.php">Wigs</a></li>
+            <li class="nav-link"><a class="text-black" href="gifts.php">Hair Extensions</a></li>
+            <li class="nav-link"><a class="text-black" href="candels.php">Hair Care</a></li>
+            <li class="nav-link"><a class="text-black" href="#">Skin Care</a></li>
+            <li class="nav-link"><a class="text-black" href="#">Makeup</a></li>
+            <li class="nav-link"><a class="text-black" href="accessories.php">Health & Wellness</a></li>
+            <li class="nav-link"><a class="text-black" href="accessories.php">Perfumes</a></li>
+            <li class="nav-link"><a class="text-black" href="accessories.php">Men</a></li>
+        </ul>
+    </div>
+</header>
